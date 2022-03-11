cdfd48ae260a20c157fe2b9938a8955629312595
Renato Kenji Aguena <aguena.kenji@gmail.com>
2022-03-11 17:34:37 -0300

    refactor: move data layer interfaces to folders

--- src/main/decorators/logs.spec.ts
+++ src/main/decorators/logs.spec.ts
@@ -1,5 +1,5 @@
 import { AccountModel } from '../../domain/models/account'
-import { LogErrorRepository } from './../../data/protocols/log-error-repository'
+import { LogErrorRepository } from '../../data/protocols/db/log-error-repository'
 import { serverError, ok } from '../../presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from './../../presentation/protocols'
 import { LogControllerDecorator } from './logs'
